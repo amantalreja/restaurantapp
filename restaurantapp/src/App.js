b47798bbe97135166ff22763fe01aeff11f0d423
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-06-07 19:13:19 +0000

    first commit

---
+++ restaurantapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+class App{
+
+    render(){
+        return (<div>
+            Restaurant
+        </div>)
+    }
+}
+
+export default App
---
+++ restaurantapp/src/index.js
@@ -0,0 +1,5 @@
+import {React} from 'react'
+import { ReactDOM } from 'react'
+import './index.css'
+
+ReactDOM.render(<App/>,document.getElementById('root'));
