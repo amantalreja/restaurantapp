dcd27bf4811675e149b48b0116944386e0619d67
Aman Talreja <64047297+amantalreja@users.noreply.github.com>
2023-06-07 19:51:54 +0000

    second

--- restaurantapp/src/App.js
+++ restaurantapp/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 
-class App{
-
-    render(){
-        return (<div>
-            Restaurant
+const App=()=>{
+        return (<div className="w-screen h-screen flex items-center justify-center ">
+           <h1>Restaurant</h1> 
         </div>)
-    }
+    
 }
 
 export default App
